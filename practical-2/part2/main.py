890b40347aa2ecd8d27603f6adc47e1ad5f57058
Angel Bujalance Gómez <58339896+angelbujalance@users.noreply.github.com>
2023-12-01 18:40:04 +0100

    Update main.py

--- practical-2/part2/main.py
+++ practical-2/part2/main.py
@@ -115,6 +115,8 @@ def parse_option():
     args.num_workers = min(args.num_workers, os.cpu_count())
 
     args.filename = "{}_{}_{}_{}_{}_{}_lr_{}_decay_{}_bsz_{}_warmup_{}_trial_{}".format(
+        args.prompt_type,
+        args.injection_layer,
         args.method,
         args.prompt_size,
         args.dataset,
